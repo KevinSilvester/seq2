cd0580ad0b0f81898b32f3a942832b06ae298193
Kevin Silvester <kevinsilvester05@gmail.com>
2024-04-09 23:32:20 +0100

    feat: add unary tokens

--- src/tokens.rs
+++ src/tokens.rs
@@ -8,19 +8,29 @@ pub enum Op {
     Div,
     Pow,
     Mod,
+    UnarySub,
+    UnaryAdd,
+}
+
+impl Op {
+    pub fn precedence(&self) -> u8 {
+        match self {
+            Op::Add | Op::Sub => 1,
+            Op::Mul | Op::Div | Op::Mod => 2,
+            Op::Pow => 3,
+            Op::UnaryAdd | Op::UnarySub => 4,
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 #[rustfmt::skip]
 pub enum TokenKind {
     // Misc
-    // Space,
-    // START,
-    // END,
     Comma,
 
     // Numbers
-    Int { val: u32 },
+    Int { value: i64 },
 
     // Math operations
     Math(Op),
@@ -39,32 +49,6 @@ pub enum TokenKind {
     RngMutArg,    // @
 }
 
-// impl fmt::Display for TokenKind {
-//     #[rustfmt::skip]
-//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-//         match self {
-//             TokenKind::Int { str_val, .. } => write!(f, "{str_val}"),
-//             // TokenKind::Space        => write!(f, " "),
-//             TokenKind::Comma        => write!(f, ","),
-//             TokenKind::MathAdd      => write!(f, "+"),
-//             TokenKind::MathSub      => write!(f, "-"),
-//             TokenKind::MathMul      => write!(f, "*"),
-//             TokenKind::MathDiv      => write!(f, "/"),
-//             TokenKind::MathPow      => write!(f, "^"),
-//             TokenKind::MathMod      => write!(f, "%"),
-//             TokenKind::LParen       => write!(f, "("),
-//             TokenKind::RParen       => write!(f, ")"),
-//             TokenKind::LSquiggly    => write!(f, "{{"),
-//             TokenKind::RSquiggly    => write!(f, "}}"),
-//             TokenKind::RngInclusive => write!(f, "..="),
-//             TokenKind::RngExclusive => write!(f, ".."),
-//             TokenKind::RngStep      => write!(f, "s:"),
-//             TokenKind::RngMutation  => write!(f, "m:"),
-//             TokenKind::RngMutArg    => write!(f, "@"),
-//         }
-//     }
-// }
-
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub struct Token {
     pub kind: TokenKind,
@@ -76,38 +60,6 @@ impl Token {
         Self { kind, span }
     }
 }
-
-// #[derive(Debug)]
-// pub struct TokenIter<'a> {
-//     tokens: &'a [Token],
-//     index: usize,
-// }
-
-// impl<'a> TokenIter<'a> {
-//     pub fn new(tokens: &'a [Token]) -> Self {
-//         Self { tokens, index: 0 }
-//     }
-// }
-
-// impl<'a> Iterator for TokenIter<'a> {
-//     type Item = &'a Token;
-
-//     fn next(&mut self) -> Option<Self::Item> {
-//         if self.tokens.is_empty() {
-//             return None;
-//         }
-
-//         let token = &self.tokens[self.index];
-//         self.index += 1;
-//         Some(token)
-//     }
-// }
-
-// impl fmt::Display for Token {
-//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-//         write!(f, "{}", self.kind)
-//     }
-// }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Span {
